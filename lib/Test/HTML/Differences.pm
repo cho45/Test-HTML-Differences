3b02a13bd3d268777de79d49bbf4a6d119382272
cho45 <cho45@lowreal.net>
2010-12-04 07:54:13 +0900

    init

---
+++ lib/Test/HTML/Differences.pm
@@ -0,0 +1,36 @@
+package Test::HTML::Differences;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Test::HTML::Differences - 
+
+=head1 SYNOPSIS
+
+  use Test::HTML::Differences;
+
+
+=head1 DESCRIPTION
+
+Test::HTML::Differences is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
